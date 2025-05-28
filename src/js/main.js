f8cb756f46acc76b5df84099691b58e458bb5c57
Zsuzsanna Benkö <Susi-88@users.noreply.github.com>
2025-05-28 08:33:02 +0200

    Declare variables and generate input data in JS

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+// Variables
+// Eingabedaten holen
+let titel = prompt("Titel:");
+    console.log(titel);
+let typ = prompt("Typ (Einnahme oder Ausgabe):");
+    console.log(typ);
+let betrag = prompt("Betrag (in Cent):");
+    console.log(betrag);
+let datum = prompt("Datum (jjjj-mm-tt):");
+    console.log(datum);
+
+// Gesammtbilant
+let einnahmen;
+let ausgaben;
+let bilanz;
