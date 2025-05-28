bfc3be9802160178d420409867e9f329144138b8
Zsuzsanna Benkö <Susi-88@users.noreply.github.com>
2025-05-28 15:30:37 +0200

    Add template strings and operators

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,29 @@
-// Variables
+// VARIABELS
+
 // Eingabedaten holen
-let titel = prompt("Titel:");
-    console.log(titel);
-let typ = prompt("Typ (Einnahme oder Ausgabe):");
-    console.log(typ);
-let betrag = prompt("Betrag (in Cent):");
-    console.log(betrag);
-let datum = prompt("Datum (jjjj-mm-tt):");
-    console.log(datum);
+let titel_1 = prompt("Titel:");
+    console.log(`Titel: ${titel_1}`);
+let typ_1 = prompt("Typ (Einnahme oder Ausgabe):");
+    console.log(`Typ: ${typ_1}`);
+let betrag_1 = parseInt(prompt("Betrag (in Cent):"));
+    console.log(`Betrag: ${betrag_1}`);
+let datum_1 = prompt("Datum (jjjj-mm-tt):");
+    console.log(`Datum: ${datum_1}`);
+
+let titel_2 = prompt("Titel:");
+    console.log(`Titel: ${titel_2}`);
+let typ_2 = prompt("Typ (Einnahme oder Ausgabe):");
+    console.log(`Typ: ${typ_2}`);
+let betrag_2 = parseInt(prompt("Betrag (in Cent):"));
+    console.log(`Betrag: ${betrag_2}`);
+let datum_2 = prompt("Datum (jjjj-mm-tt):");
+    console.log(`Datum: ${datum_2}`);
 
 // Gesammtbilant
 let einnahmen;
 let ausgaben;
-let bilanz;
+let bilanz = betrag_1 + betrag_2;
+    console.log(`Bilanz: ${bilanz} cent`);
+
+let positiv = bilanz >=0;
+    console.log(`Bilanz: ist positiv: ${positiv}`);
