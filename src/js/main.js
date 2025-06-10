0fd8aaab618599a018c67027712bcb5cc0ebfad5
Zsuzsanna Benkö <Susi-88@users.noreply.github.com>
2025-06-10 16:02:05 +0200

    Switch to functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,62 @@
 // VARIABELS
 // Gesamtbilanz anlegen
-let einnahmen = 0;
-let ausgaben = 0;
-let bilanz = 0;
+let einnahmen = 0,
+    ausgaben = 0,
+    bilanz = 0,
+    titel, 
+    typ, 
+    betrag, 
+    datum;
 
 // Eingabedaten holen
-let titel_1 = prompt("Titel:");
-let typ_1 = prompt("Typ (Einnahme oder Ausgabe):");
-let betrag_1 = parseInt(prompt("Betrag (in Cent):"));
-let datum_1 = prompt("Datum (jjjj-mm-tt):");
-    
-console.log(`Titel: ${titel_1}
-Typ: ${typ_1}
-Betrag: ${betrag_1}
-Datum: ${datum_1}`);
+const eintrag_erfassen = function(){
+    titel = prompt("Titel:");
+    typ = prompt("Typ (Einnahme oder Ausgabe):");
+    betrag = parseInt(prompt("Betrag (in Cent):"));
+    datum = prompt("Datum (jjjj-mm-tt):");
+};
 
-if (typ_1 === "ein") {
-    einnahmen = einnahmen + betrag_1;
-    bilanz = bilanz + betrag_1;
-} else if (typ_1 === "aus") {
-    ausgaben = ausgaben + betrag_1;
-    bilanz = bilanz - betrag_1;
-} else {
-    console.log("Falscher Typ");
-}
 
-let titel_2 = prompt("Titel:");
-let typ_2 = prompt("Typ (Einnahme oder Ausgabe):");
-let betrag_2 = parseInt(prompt("Betrag (in Cent):"));
-let datum_2 = prompt("Datum (jjjj-mm-tt):");
+// Eintrag Ausgeben
+const eintrag_ausgeben = function(titel, typ, betrag, datum){
+    console.log(`Titel: ${titel}
+Typ: ${typ}
+Betrag: ${betrag}
+Datum: ${datum}`
+    );
+};
 
-console.log(`Titel: ${titel_2}
-Typ: ${typ_2}
-Betrag: ${betrag_2}
-Datum: ${datum_2}`);
-
-if (typ_2 === "ein") {
-    einnahmen = einnahmen + betrag_2;
-    bilanz = bilanz + betrag_2;
-} else if (typ_2 === "aus") {
-    ausgaben = ausgaben + betrag_2;
-    bilanz = bilanz - betrag_2;
-} else {
-    console.log("Falscher Typ");
-}
+// Eintrag mmit Gesamtbilanz Verrechnen
+const eintrag_mit_gesamtbilanz_verrechnen = function(typ, betrag){
+    if (typ === "ein") {
+        einnahmen = einnahmen + betrag;
+        bilanz = bilanz + betrag;
+    } else if (typ === "aus") {
+        ausgaben = ausgaben + betrag;
+        bilanz = bilanz - betrag;
+    } else {
+        console.log("Falscher Typ");
+    }
+};
 
 // Gesamtbilanz ausgaben
-let positiv = bilanz >=0;
+const gesamtbilanz_ausgeben = function(einnahmen, ausgaben, bilanz){
+    console.log(`Einnahmen: ${einnahmen} cent
+    Ausgaben: ${ausgaben} cent
+    Bilanz: ${bilanz} cent
+    Bilanz ist positiv: ${bilanz >=0}`
+    );
+};
 
-console.log(`Einnahmen: ${einnahmen} cent
-Ausgaben: ${ausgaben} cent
-Bilanz: ${bilanz} cent
-Bilanz ist positiv: ${positiv}`);
+
+// Eintrag hinzufügen
+const eintrag_hinzufuegen = function(){
+    eintrag_erfassen();
+    eintrag_mit_gesamtbilanz_verrechnen(typ, betrag);
+    eintrag_ausgeben(titel, typ, betrag, datum);
+    gesamtbilanz_ausgeben(einnahmen, ausgaben, bilanz);
+};
+
+eintrag_hinzufuegen();
+eintrag_hinzufuegen();
+eintrag_hinzufuegen();
